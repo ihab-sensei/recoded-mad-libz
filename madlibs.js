189ff5cb46f3e4a272103012a25da621ebdc8c27
fadime94 <fadime.ozdemir11@gmail.com>
2020-08-12 14:17:33 +0300

    add arr with word obj and create inputs

--- madlibs.js
+++ madlibs.js
@@ -30,14 +30,39 @@ function parseStory(rawStory) {
   // Your code here.
   const storyWords = rawStory.split(' ');
   console.log(storyWords);
-  //let wordInBrackets = new RegExp(/\[.\]/);
-  const adjective = /\[adjective\]/;//redo
-  const matches = storyWords.filter(value => /\[adjective\]/.test(value));//delete
+  //all regex
+  const allMatcher = /[a-z]+\[[a-z]+\]/g;
+  const adjectives = /[a-z]+\[[a]+\]/g;
+  const nationality = /[a-z]+\[na+\]/g;
+  const person = /[a-z]+\[p+\]/g;
+  const verb = /[a-z]+\[v+\]/g;
+  const nouns = /[a-z]+\[[n]+\]/g;
+  const numbers = /[a-z]+\[nu+\]/g;
+  const shape = /[a-z]+\[[s]+\]/g;
+  const foods =  /[a-z]+\[[f]+\]/g;
+  // const arrayOfRegex = [adjectives, nationality, person, verb, nouns, numbers, shape, foods];
+  //const matches = storyWords.filter(value => foods.test(value));
+  //console.log(matches);
+  const objsOfWords= [];
+  for ( const word of storyWords){
+    if ((word.match(allMatcher)) !== null){
+      objsOfWords.push({word: word, pos:word })
+    }else {
+      objsOfWords.push({word: word})
+    }
+  }
+  console.log(objsOfWords)
+  /* 
+  const wordObjs = storyWords.map(word => {
+    //console.log(word.match(allMatcher))
+    if ((word.match(allMatcher)) !== null){
+      objsOfWords.push({word: word, pos:word })
+    }
+    }); */
   
-  console.log(matches);//delete
-  console.log(adjective.test(storyWords[5]));//now this doesnt work because string values different
 
-  return rawStory; 
+  
+  return objsOfWords; 
 }
 
 /**
@@ -54,5 +79,37 @@ function parseStory(rawStory) {
 getRawStory()
   .then(parseStory)
   .then((processedStory) => {
-    console.log(processedStory);
+    const previewDiv = document.querySelector(".madLibsPreview");
+    const editDiv = document.querySelector(".madLibsEdit");
+    
+    // word is : {word : "some word", pos: "adjective"}
+    for (let word of processedStory){
+      //if (pos in word)
+      // if (pos in word){
+      //   previewDiv.innerHTML +=`<input type="text" placeholder=${word.pos}>`
+      // }
+      console.log(word.pos)
+
+      
+      
+      if (word.word !== "." && word.word !== ","){
+        //console.log(word.word);
+        // previewDiv.innerHTML += ` <span>${word.word}</span>`;
+       if (word.pos){
+        editDiv.innerHTML +=` <input type="text" placeholder=${word.pos}>`;
+        previewDiv.innerHTML += ` <span>${word.word}</span>`;
+       }else {
+        previewDiv.innerHTML += ` ${word.word}`;
+        editDiv.innerHTML += ` ${word.word}`;
+       }
+      } else if (word.word === "." || word.word === ",") {
+        previewDiv.innerHTML += `${word.word}`;
+        //console.log("punctuation");
+      }
+    }
+    
+    console.log('processedStory  :  '+processedStory);
+
   });
+
+  
