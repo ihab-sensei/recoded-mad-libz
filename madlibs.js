3693fbbb4fc6c8447a6703365e2a25e0ec20e02b
ihab-sensei <ib-mashhadani@hotmail.com>
2020-08-18 21:29:01 +0300

    added music

--- madlibs.js
+++ madlibs.js
@@ -1,15 +1,14 @@
 function parseStory(rawStory) {
   // Your code here.
-  const storyWords = rawStory.split(' ');
+  const storyWords = rawStory.split(" ");
 
   //all regex
   const rex = /\[.*\]/gi;
-  const objsOfWords = storyWords.map(word => {
+  const objsOfWords = storyWords.map((word) => {
     const wordOnly = word.replace(word.match(rex), "");
     let posRe = word.match(rex);
 
     if (posRe === null) {
-
       return { word: wordOnly };
     }
     posRe = posRe[0];
@@ -37,11 +36,10 @@ function parseStory(rawStory) {
 
       case "[nu]":
         return { word: wordOnly, pos: "number" };
-
     }
-  })
+  });
 
-  console.log(objsOfWords)
+  console.log(objsOfWords);
   return objsOfWords;
 }
 
@@ -53,9 +51,7 @@ getRawStory()
 
     // word is : {word : "some word", pos: "adjective"}
     for (let word of processedStory) {
-
       if (word.word !== "." && word.word !== ",") {
-
         if (word.pos) {
           const input = document.createElement("input");
           const whiteSpace = document.createElement("span");
@@ -67,7 +63,6 @@ getRawStory()
           editParagraph.appendChild(whiteSpace);
           editParagraph.appendChild(input);
 
-
           const whiteSpace2 = document.createElement("span");
           whiteSpace2.innerText = " ";
           const output = document.createElement("mark");
@@ -77,7 +72,6 @@ getRawStory()
           previewParagraph.appendChild(output);
 
           input.addEventListener("input", (e) => {
-
             output.innerHTML = input.value;
             //update the obj word in arrOfObj
             if (input.value) {
@@ -86,25 +80,24 @@ getRawStory()
               word.word = input.value;
 
               output.setAttribute("class", "filled");
-
             } else {
-
               input.removeAttribute("class", "filled");
-              output.removeAttribute("class", "filled")
+              output.removeAttribute("class", "filled");
               word.word = `${word.pos}`;
-              output.innerText = `${word.word}`
+              output.innerText = `${word.word}`;
             }
-          })
-
-        } else {//normal word + space before word
-          const p = document.createElement("span")
-          p.innerText = ` ${word.word}`
-          previewParagraph.appendChild(p)
-          const p2 = document.createElement("span")
-          p2.innerText = ` ${word.word}`
-          editParagraph.appendChild(p2)
+          });
+        } else {
+          //normal word + space before word
+          const p = document.createElement("span");
+          p.innerText = ` ${word.word}`;
+          previewParagraph.appendChild(p);
+          const p2 = document.createElement("span");
+          p2.innerText = ` ${word.word}`;
+          editParagraph.appendChild(p2);
         }
-      } else { //dot and comma with no space
+      } else {
+        //dot and comma with no space
         const p = document.createElement("span");
         p.innerText = `${word.word}`;
         previewParagraph.appendChild(p);
@@ -113,23 +106,21 @@ getRawStory()
         p2.innerText = `${word.word}`;
         editParagraph.appendChild(p2);
       }
-
     } // THE END OF THE LOOP
 
     //keypress event - next input
     const inputFields = document.querySelectorAll("input");
     for (let i = 0; i < inputFields.length; i++) {
-      inputFields[i].addEventListener("keypress", e => {
+      inputFields[i].addEventListener("keypress", (e) => {
         if (e.key === "Enter") {
           if (i === inputFields.length - 1) {
-            inputFields[0].focus()
+            inputFields[0].focus();
           } else {
             inputFields[i + 1].focus();
           }
         }
-      })
+      });
     }
-
 
     //button save-load-reset
     const saveBtn = document.querySelector(".save");
@@ -139,42 +130,30 @@ getRawStory()
 
     //initialize the toast
     let option = false;
-    let toastElList = [].slice.call(document.querySelectorAll('.toast'))
-    console.log(toastElList)
+    let toastElList = [].slice.call(document.querySelectorAll(".toast"));
     let toastList = toastElList.map(function (toastEl) {
-      return new bootstrap.Toast(toastEl, option)
+      return new bootstrap.Toast(toastEl, option);
     });
 
-    saveBtn.addEventListener("click", e => {
-      let base = 0
+    saveBtn.addEventListener("click", (e) => {
+      let base = 0;
       for (let i = 0; i < inputFields.length; i++) {
-        /*  if(inputFields[i]=""){
-           toastList[4].show()
-         }else {
-           const data = inputFields[i].value;
-         localStorage.setItem(`inputNum${i}`, data)
-         toastList[0].show()
-         }
-  */
         if (inputFields[i].value) {
           base++;
           const data = inputFields[i].value;
-          localStorage.setItem(`inputNum${i}`, data)
-          toastList[0].show()
-        }else if (base === 0) {
-        toastList[4].show()
+          localStorage.setItem(`inputNum${i}`, data);
+          toastList[0].show();
+        } else if (base === 0) {
+          toastList[4].show();
+        }
       }
-
-      }
-
     });
 
-    loadBtn.addEventListener("click", e => {
+    loadBtn.addEventListener("click", (e) => {
       if (localStorage.length === 0) {
-        toastList[3].show()
+        toastList[3].show();
       } else {
         for (let i = 0; i < inputFields.length; i++) {
-
           const data = localStorage.getItem(`inputNum${i}`);
           if (data) {
             inputFields[i].value = data;
@@ -183,11 +162,11 @@ getRawStory()
             marks[i].setAttribute("class", "filled");
           }
         }
-        toastList[1].show()
+        toastList[1].show();
       }
     });
 
-    resetBtn.addEventListener("click", e => {
+    resetBtn.addEventListener("click", (e) => {
       for (let i = 0; i < inputFields.length; i++) {
         inputFields[i].value = "";
         marks[i].innerText = inputFields[i].placeholder;
@@ -195,9 +174,11 @@ getRawStory()
         marks[i].removeAttribute("class", "filled");
       }
 
-      localStorage.clear()
-      toastList[2].show()
+      localStorage.clear();
+      toastList[2].show();
     });
 
+    const music = document.querySelector("#music");
+    const getStartedBtn = document.querySelector(".get-started");
+    getStartedBtn.addEventListener("click", () => music.play());
   });
-
